7a18816e7fad513a1e87a6da17d76ebb30f2627b
t0mbb <103032609+t0mbb@users.noreply.github.com>
2024-10-13 23:55:08 +0700

    test feedback

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -125,37 +125,9 @@ let handleMessage = async (sender_psid, received_message) => {
     if (received_message.text) {
         // Create the payload for a basic text message
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            "text": `Hiện tại tin nhắn của bạn đang không trong Form của hệ thống vui lòng bấm Bắt Đầu để nhắn lại | \n Hoặc bạn muốn tư vấn trực tiếp vui lòng chọn Hỏi Đáp ! `
         }
-    } else if (received_message.attachments) {
-        // Get the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
-        response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Is this the right picture?",
-                        "subtitle": "Tap a button to answer.",
-                        "image_url": attachment_url,
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
-                            }
-                        ],
-                    }]
-                }
-            }
-        }
-    }
+    } 
 
     // Sends the response message
     await chatbotService.sendMessage(sender_psid, response);
