ed183a7caebd544ceea47187e3c5b2ad2bf3c6a7
t0mbb <103032609+t0mbb@users.noreply.github.com>
2024-10-14 16:01:17 +0700

    test

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -65,7 +65,7 @@ let postWebhook = (req, res) => {
 
                 // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
+            console.log("Body : ",webhook_event);
 
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
