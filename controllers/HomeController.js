c0fb0a94142d9f8245306922b4136389cba55cf7
t0mbb <103032609+t0mbb@users.noreply.github.com>
2024-10-22 20:21:18 +0700

    1234565

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -137,42 +137,12 @@ let handleMessage = async (sender_psid, received_message) => {
             await chatbotService.sendLookupOrder(sender_psid);
         }
     }
-    // let response ;
-    // if (received_message.text) {
-    //     // Create the payload for a basic text message
-    //     response = {
-    //         "text": `Cảm ơn bạn vì đã gửi tin nhắn đến Empty Arena Billiards !\nChúng tôi sẽ phản hồi lại bạn khi Online!`
-    //     }
-    // } else if (received_message.attachments) {
-    //     // Get the URL of the message attachment
-    //     let attachment_url = received_message.attachments[0].payload.url;
-    //     response = {
-    //         "attachment": {
-    //             "type": "template",
-    //             "payload": {
-    //                 "template_type": "generic",
-    //                 "elements": [{
-    //                     "title": "Is this the right picture?",
-    //                     "subtitle": "Tap a button to answer.",
-    //                     "image_url": attachment_url,
-    //                     "buttons": [
-    //                         {
-    //                             "type": "postback",
-    //                             "title": "Yes!",
-    //                             "payload": "yes",
-    //                         },
-    //                         {
-    //                             "type": "postback",
-    //                             "title": "No!",
-    //                             "payload": "no",
-    //                         }
-    //                     ],
-    //                 }]
-    //             }
-    //         }
-    //     }
-    // }
-    // await chatbotService.sendMessage(sender_psid, response);
+ 
+    if (received_message.text) {
+        // Create the payload for a basic text message
+        await chatbotService.requestTalkToAgent(sender_psid);
+    } 
+    await chatbotService.sendMessage(sender_psid);
 
 };
 
